822fe03025932ad41acc9b64f23467983acf5e2c
John Berthels <john.berthels@gmail.com>
2021-02-22 20:52:56 +0000

    Multicreeches and food

--- creech.go
+++ creech.go
@@ -9,31 +9,67 @@ import (
 )
 
 type Game struct {
-	width, height int
+	size Pos
 
 	creeches []*Creech
+	food     []*Food
 
 	renderer render.Renderer
 }
 
 func NewGame(r render.Renderer) *Game {
 	return &Game{
-		width:    10,
-		height:   10,
+		size:     Pos{20, 20},
 		renderer: r,
 	}
 }
 
 func (g *Game) Init() error {
-	origin := Pos{0, 0}
-	bob := NewCreech("bob", origin)
 
+	g.AddCreeches()
+	g.AddFood()
+	return g.renderer.Init(g.size.X, g.size.Y)
+}
+
+func (g *Game) AddCreeches() {
+	bob := NewCreech("bob", Pos{0, 0})
 	g.creeches = append(g.creeches, bob)
-	return g.renderer.Init(g.width, g.height)
+
+	alice := NewCreech("alice", Pos{2, 2})
+	g.creeches = append(g.creeches, alice)
+}
+
+func (g *Game) AddFood() {
+	numFood := 5
+	for i := 0; i < numFood; i++ {
+		value := rand.Intn(10)
+		f := NewFood(value, g.randomEmptyPos())
+		g.food = append(g.food, f)
+	}
+}
+
+// TODO: at a certain point, we'll want to avoid looping over everything to do this
+func (g *Game) randomEmptyPos() Pos {
+RANDOM_POSITION:
+	for {
+		p := Pos{rand.Intn(g.size.X), rand.Intn(g.size.Y)}
+		p = moduloPos(p, g.size)
+		for _, c := range g.creeches {
+			if c.Pos().Equal(p) {
+				continue RANDOM_POSITION
+			}
+		}
+		for _, f := range g.food {
+			if f.Pos().Equal(p) {
+				continue RANDOM_POSITION
+			}
+		}
+		return p
+	}
 }
 
 func (g *Game) Run() error {
-	tickDur := time.Second
+	tickDur := 100 * time.Millisecond
 	tickCh := time.Tick(tickDur)
 	ticks := 0
 
@@ -50,6 +86,7 @@ func (g *Game) Run() error {
 
 func (g *Game) Update() {
 	for _, creech := range g.creeches {
+		creech.ModuloPos(g.size)
 		creech.MakePlan()
 	}
 	for _, creech := range g.creeches {
@@ -63,19 +100,32 @@ func (g *Game) Draw(ticks int) error {
 	if err != nil {
 		return fmt.Errorf("StartFrame: %w", err)
 	}
+
+	for _, f := range g.food {
+		p := f.Pos()
+		err = r.DrawAt(p.X, p.Y, f)
+		if err != nil {
+			return fmt.Errorf("DrawAt: %w", err)
+		}
+	}
+
 	for _, creech := range g.creeches {
 		p := creech.Pos()
 		err = r.DrawAt(p.X, p.Y, creech)
+		if err != nil {
+			return fmt.Errorf("DrawAt: %w", err)
+		}
 	}
-	if err != nil {
-		return fmt.Errorf("DrawAt: %w", err)
-	}
+
 	err = r.FinishFrame()
 	if err != nil {
 		return fmt.Errorf("FinishFrame: %w", err)
 	}
 
 	fmt.Printf("%d ticks\n", ticks)
+	for _, creech := range g.creeches {
+		fmt.Printf("%s\n", creech)
+	}
 
 	return nil
 }
@@ -94,11 +144,36 @@ func NewCreech(name string, pos Pos) *Creech {
 	}
 }
 
+func (c *Creech) String() string {
+	return fmt.Sprintf("%s: %s %s", c.name, c.pos, c.facing)
+}
+
 func (c *Creech) Pos() Pos {
 	return c.pos
 }
 
 func (c *Creech) MakePlan() {
+}
+
+func (c *Creech) ModuloPos(worldSize Pos) {
+	c.pos = moduloPos(c.pos, worldSize)
+}
+
+func moduloPos(p Pos, worldSize Pos) Pos {
+	q := p // Struct copy
+	if q.X > worldSize.X/2 {
+		q.X -= worldSize.X
+	}
+	if q.X <= -worldSize.X/2 {
+		q.X += worldSize.X
+	}
+	if q.Y > worldSize.Y/2 {
+		q.Y -= worldSize.Y
+	}
+	if q.Y <= -worldSize.Y/2 {
+		q.Y += worldSize.Y
+	}
+	return q
 }
 
 func (c *Creech) DoPlan() {
@@ -112,5 +187,40 @@ func (c *Creech) DoPlan() {
 }
 
 func (c *Creech) Screen() byte {
-	return 'X'
+	switch c.facing {
+	case North:
+		return '^'
+	case East:
+		return '>'
+	case South:
+		return 'v'
+	case West:
+		return '<'
+	}
+	panic("wtf")
+}
+
+type Food struct {
+	value int
+	pos   Pos
+}
+
+func NewFood(v int, p Pos) *Food {
+	return &Food{value: v, pos: p}
+}
+
+func (f *Food) Pos() Pos {
+	return f.pos
+}
+
+func (f *Food) Screen() byte {
+	if f.value < 3 {
+		return '.'
+	} else if f.value < 3 {
+		return 'o'
+	} else if f.value < 6 {
+		return 'O'
+	} else {
+		return '*'
+	}
 }
--- pos.go
+++ pos.go
@@ -6,6 +6,10 @@ type Pos struct {
 	X, Y int
 }
 
+func (p Pos) Equal(q Pos) bool {
+	return p.X == q.X && p.Y == q.Y
+}
+
 func (p Pos) Add(q Pos) Pos {
 	return Pos{p.X + q.X, p.Y + q.Y}
 }
@@ -14,12 +18,31 @@ func (p Pos) Move(d Dir) Pos {
 	return p.Add(Pos(d))
 }
 
+func (p Pos) String() string {
+	return fmt.Sprintf("[%d,%d]", p.X, p.Y)
+}
+
 type Dir Pos
 
 var North = Dir{0, 1}
 var East = Dir{1, 0}
 var South = Dir{0, -1}
 var West = Dir{-1, 0}
+
+func (d Dir) String() string {
+	switch d {
+	case North:
+		return "N"
+	case East:
+		return "E"
+	case South:
+		return "S"
+	case West:
+		return "W"
+	default:
+		panic(fmt.Sprintf("wtf: %v", d))
+	}
+}
 
 func (d Dir) TurnRight() Dir {
 	switch d {
